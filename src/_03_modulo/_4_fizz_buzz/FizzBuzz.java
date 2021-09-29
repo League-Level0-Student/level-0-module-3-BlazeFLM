6acd94b1fd3de21d22e6b6da56d2b2a89ee08298
student <student@ip-172-31-62-140.us-west-2.compute.internal>
2021-09-28 18:27:49 -0700

    6:27 PM 9/28/2021

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -22,11 +22,17 @@ package _03_modulo._4_fizz_buzz;
 public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int i = 0; i < 20; i++) {
-			if(i%3 == 0) {
+
+			if (i % 3 == 0 & i % 5 == 0) {
+				System.out.println("Fizzbuzz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (i % 3 == 0) {
 				System.out.println("Fizz");
+			} else {
+				System.out.println(i);
 			}
-			System.out.println(i);
-			 
+
 		}
 	}
 }
